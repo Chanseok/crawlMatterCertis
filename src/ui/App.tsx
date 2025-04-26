8a304156a04c43fd39733e53d451fac0f049c9fc
Chanseok <hi007chans@gmail.com>
2025-04-26 22:03:41 +0900

    feat: implement Chart component for data visualization and integrate it into App component

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,16 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { useStatistics } from './useStatistics'
+import { Chart } from './Charts';
 
 function App() {
   const [count, setCount] = useState(0);
   const statistics = useStatistics(10);
+  const cpuUsage = useMemo(() => statistics.map(stat => stat.cpuUsage), [statistics]);
 
-  console.log(statistics);
+  // console.log(statistics);
 
   return (
-    <>
+    <div className="App">
+      <div style={{ height: 120 }}> 
+        <Chart data={cpuUsage} maxDataPoints={10}/>
+      </div>
       <div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -28,7 +33,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
